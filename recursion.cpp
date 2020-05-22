6438566d640749856d49853850d89bb2ca3c771c
pandavshyam <pandavshyam@gmail.com>
2020-05-23 01:43:15 +0530

    Added Recursion Program

---
+++ recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int x){
+    if ( x == 1 ){
+        return 1;
+    } else{
+         return x * factorial(x-1);
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter number to find factorial: " << endl;
+    cin >> n;
+    cout << factorial(n) << endl;
+}
