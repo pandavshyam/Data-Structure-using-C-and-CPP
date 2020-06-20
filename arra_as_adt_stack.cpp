f0d2adef60976b705c51e09e5935b98b0d05fb54
pandavshyam <pandavshyam@gmail.com>
2020-06-21 01:01:25 +0530

    Final Array as ADT using Stack

--- arra_as_adt_stack.cpp
+++ arra_as_adt_stack.cpp
@@ -126,9 +126,142 @@ float Avg(struct Array arr){
     return (float)Sum(arr) / arr.length;
 }
 
+void Reverse(struct Array *arr){
+    int *B, i, j;
+    B = new int[arr->length];
+
+    for(i = arr->length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = arr->A[i];
+    for ( i = 0; i < arr->length; i++)
+        arr->A[i] = B[i];
+    delete[] B;
+}
+void Reverse1(struct Array *arr){
+    int i,j;
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
+        Swap(&arr->A[i], &arr->A[j]);
+}
+
+void InsertSort(struct Array *arr, int x){
+    int i = arr->length - 1;
+    if (arr->size == arr->length)
+        return;
+    while (i >= 0 && arr->A[i] > x){
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr){
+    int i;
+    for (i = 0; i < arr.length - 1; i++){
+        if (arr.A[i] > arr.A[i+1])
+            return 0;
+    }
+    return 1;
+}
+
+void REarrange(struct Array *arr){
+    int i = 0,j = arr->length - 1;
+    while (i < j){
+        while (arr->A[i] < 0) {i++;};
+        while (arr->A[j] > 0) {j--;};
+        if (i < j)
+            Swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
+struct Array* Merge(struct Array *arr1, struct Array *arr2){
+    int i =0, j = 0, k = 0;
+    struct Array *arr3 = new struct Array[sizeof(struct Array)];
+    while ( i < arr1->length && j < arr2->length){
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    for(; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for(; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+    return arr3;
+}
+
+struct Array* Union(struct Array *arr1, struct Array *arr2){
+    int i =0, j = 0, k = 0;
+    struct Array *arr3 = new struct Array[sizeof(struct Array)];
+    while ( i < arr1->length && j < arr2->length){
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr1->A[i] > arr2->A[j])
+            arr3->A[k++] = arr2->A[j++];
+        else{
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+    for(; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for(; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
+struct Array* Intersection(struct Array *arr1, struct Array *arr2){
+    int i =0, j = 0, k = 0;
+    struct Array *arr3 = new struct Array[sizeof(struct Array)];
+    while ( i < arr1->length && j < arr2->length){
+        if (arr1->A[i] < arr2->A[j])
+            i++;
+        else if (arr1->A[i] > arr2->A[j])
+            j++;
+        else{
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
+struct Array* Difference(struct Array *arr1, struct Array *arr2){
+    int i =0, j = 0, k = 0;
+    struct Array *arr3 = new struct Array[sizeof(struct Array)];
+    while ( i < arr1->length && j < arr2->length){
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr1->A[i] > arr2->A[j])
+            j++;
+        else{
+            i++;
+            j++;
+        }
+    }
+
+    for(; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
 int main(){
-    struct Array arr = {{1,2,3,4,5},10,5};
-    cout << RBinarySearch(arr, 0, 4, 2) << endl;
-    Display(arr);
+    struct Array arr1 = {{2,6,10,15,25},10,5};
+    struct Array arr2 = {{3,6,7,15,20},10,5};
+    struct Array *arr3;
+    arr3 = Difference(&arr1,&arr2);
+    Display(*arr3);
+    delete[] arr3;
     return 0;
 }
